70071415f8dfe5c532315c8676a7ea9c64eeac47
Daniel Quintero <dgquintero02@hotmail.com>
2024-05-31 10:44:45 -0500

    Load environment variables using dotenv

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # initialize a basic flask application
 
 from flask import Flask
+from config import POKE_API_URL
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Poke-berries Statistics API"
+    """
+    Home route    
+    """
+    return f"Poke-berries Statistics API - POKEAPI_URL: {POKE_API_URL}"
 
 if __name__ == '__main__':
     app.run(debug=True)
