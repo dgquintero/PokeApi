b2d09c580be815cbeed478138980397dd3f47f28
Daniel Quintero <dgquintero02@hotmail.com>
2024-06-02 23:18:07 -0500

    Adding histogram function and updating requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# initialize a basic flask application
-
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, send_file, render_template
 from flask_caching import Cache
 from config import POKE_API_URL
 from utils.data_fetch import fetch_all_berry_data
 from utils.calculations import calculate_stats
+import matplotlib.pyplot as plt
+import io
 
 
 app = Flask(__name__)
@@ -44,6 +44,32 @@ def all_berry_stats():
                 status=500,
                 mimetype="application/json"
             )
+@app.route("/berryHistogram", methods=['GET'])
+def berry_histogram():
+    """
+    Route to fetch pokemon berries histogram
+    """
+    data = fetch_all_berry_data()
+    growth_time = [berry['growth_time'] for berry in data]
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(growth_time, bins =range(min(growth_time), max(growth_time) + 1, 1), edcolor = 'black')
+    plt.title('Histogram of Berry growth time')
+    plt.xlabel('Growth time')
+    plt.ylabel('Frequency')
+
+    img_path = "static/images/berry_histogram.png"
+    plt.savefig(img_path, format='png')
+    plt.close()
+
+    return send_file(img_path, mimetype='image/png')
+
+@app.route("/showHistogram", methods=["GET"])
+def show_histogram():
+    """
+    Route to show histogram of pokemon berries
+    """
+    return render_template("histogram.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
